a1e0a1e7924fadb35442083a3a1ebf308a90dc55
AldairIAI <jorge.torres@grupo-iai.com.mx>
2024-09-03 23:30:32 -0600

    Ajua

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Aún probando para probarlo mañana</h1>
+      <p>Kemozion, mano</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
